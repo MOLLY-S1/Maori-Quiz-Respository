404815cb1940988d3865b4cc7e044c6984ef0ee9
MOLLY-S1 <98923584+MOLLY-S1@users.noreply.github.com>
2024-05-20 10:05:16 +1200

    Update 03_Scoring_V2.py

--- 03_Scoring_V2.py
+++ 03_Scoring_V2.py
@@ -1,3 +1,25 @@
 """ Version 2
 Trial 2, adds names to a file and reads off the file."""
 
+# Dictionary to store scores
+scoreboard = {}
+
+
+# Class to store scores
+class Score:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = f"{score}/10"
+        scoreboard[name] = score
+
+
+# Read off file
+def generate_scoreboard():
+    import csv
+    with open('Scoreboard.csv', newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter=',', quotechar="'")
+        for line in filereader:
+            Score(line[0], line[1])
+
+
+print(scoreboard)
