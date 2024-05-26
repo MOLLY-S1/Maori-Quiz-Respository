395df8edf2a5cea24dfbd5490d7dc5388be5374e
MOLLY-S1 <98923584+MOLLY-S1@users.noreply.github.com>
2024-05-27 09:46:00 +1200

    Update

--- 03_Scoring_V4.py
+++ 03_Scoring_V4.py
@@ -1,5 +1,5 @@
 """ Version 4
-Now using Tkinter"""
+Now using Tkinter and a function"""
 
 import random
 from tkinter import *
@@ -10,42 +10,46 @@ root = Tk()
 scoreboard = {}
 
 
-# Class to store scores from file
-class Score:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
-        scoreboard[name] = f"{score}/10"
+def scoring():
+    global scoreboard
+    # Class to store scores from file
+    class Score:
+        def __init__(self, name, score):
+            self.name = name
+            self.score = score
+            scoreboard[name] = f"{score}/10"
 
+    # Read off file
+    def generate_scoreboard():
+        import csv
+        with open('Scoreboard1.csv~', newline='') as csvfile:
+            filereader = csv.reader(csvfile, delimiter=',', quotechar="'")
+            for line in filereader:
+                Score(line[0], line[1])
 
-# Read off file
-def generate_scoreboard():
-    import csv
-    with open('Scoreboard1.csv', newline='') as csvfile:
-        filereader = csv.reader(csvfile, delimiter=',', quotechar="'")
-        for line in filereader:
-            Score(line[0], line[1])
+    # MAIN ROUTINE
+    # Get name and score
+    get_score = random.randint(1, 10)
+    get_name = input("Enter Name:")
 
+    # Write on existing file
+    file = open('Scoreboard1.csv~', 'a')
+    file.write(f"{get_name}, {get_score}\n")
+    file.close()
+    generate_scoreboard()
+
+    new_window = Toplevel(root)
+
+    # Sorting scoreboard
+    sorted_scores = sorted(scoreboard.items(), key=lambda x: int(x[1].split('/')[0]), reverse=True)
+    scoreboard = dict(sorted_scores)
+
+    Label(new_window, text="SCOREBOARD", fg="Black", font=("Times", 20, "bold")).pack()
+    place = 1
+    for key, value in sorted_scores:
+        Label(new_window, text=f"{place}. {key}: {value}").pack(side=TOP)
+        place += 1
+    root.mainloop()
 
 # MAIN ROUTINE
-# Get name and score
-get_score = random.randint(1, 10)
-get_name = input("Enter Name:")
-
-# Write on existing file
-file = open('Scoreboard1.csv', 'a')
-file.write(f"{get_name}, {get_score}\n")
-file.close()
-generate_scoreboard()
-
-new_window = Toplevel(root)
-
-# Sorting scoreboard
-sorted_scores = sorted(scoreboard.items(), key=lambda x: int(x[1].split('/')[0]), reverse=True)
-scoreboard = dict(sorted_scores)
-
-Label(new_window, text="SCOREBOARD", fg="Black", font=("Times", 20, "bold")).pack()
-for key, value in sorted_scores:
-    Label(new_window, text=f"{key}: {value}").pack(side=TOP)
-root.mainloop()
-
+scoring()
