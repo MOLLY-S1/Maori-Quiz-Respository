61db5a48f9787fe2ae072f73f120cf4c2c0a778f
MOLLY-S1 <98923584+MOLLY-S1@users.noreply.github.com>
2024-06-02 13:15:25 +1200

    Update

--- 05_Instructions_V4.py
+++ 05_Instructions_V4.py
@@ -7,15 +7,19 @@ root = Tk()
 instructions_window = Toplevel(root)
 
 
+# Return home
 def return_home():
     print("Return to home screen")
 
 
+# Start Quiz
 def start_quiz():
     print("Start Quiz")
 
 
+# Instructions
 def instructions():
+    instructions_window = Toplevel(root)
     Label(instructions_window, text="INSTRUCTIONS:\n"
                                     "When you press 'Start Quiz' you will be given two options for quiz "
                                     "type.\n"
@@ -25,8 +29,10 @@ def instructions():
                                     "answered your final score will be shown.\n"
                                     "Enter you name then go to the 'Statistics' tab "
                                     "in the home screen to see your name on the leaderboard!").pack(side=TOP)
-    Button(instructions_window, text="Return to Menu", command=return_home).pack()
-    Button(instructions_window, text="Start Quiz", command=start_quiz).pack()
+    Button(instructions_window, text="Start Quiz", command=quiz).pack()
+    Button(instructions_window, text="Return to Menu", command=exit(instructions)).pack()
+
+
 
 
 Button(root, text="Instructions", command=instructions).pack()
